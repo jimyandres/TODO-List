88ac52e6de4a7329e28f0e9decff9fe7823557d1
jimyandres <jimyandres@utp.edu.co>
2018-03-30 14:31:30 -0500

    Suppor of react-router

--- src/components/Root/index.js
+++ src/components/Root/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import App from '../App';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 const Root = ({store}) =>
   <Provider store={store}>
-    <App />
+    <BrowserRouter>
+      <Route path="/" component={App} />
+    </BrowserRouter>
   </Provider>
 
 export default Root;
