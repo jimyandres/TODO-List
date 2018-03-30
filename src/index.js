87f44431f388efde9cbeb1da2cf14bcccb667603
jimyandres <jimyandres@utp.edu.co>
2018-03-30 12:46:46 -0500

    Now the data (todos) are stored with localStore API in the browser

--- src/index.js
+++ src/index.js
@@ -6,10 +6,24 @@ import registerServiceWorker from './registerServiceWorker';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 
-import todoApp from './redux'
+import todoApp from './redux';
+import { loadState, saveState } from './localStorage';
+
+const persistedState = loadState();
+
+const store = createStore(
+  todoApp,
+  persistedState
+);
+
+store.subscribe(() =>
+  saveState({
+    todos: store.getState().todos
+  })
+);
 
 ReactDOM.render(
-  <Provider store={createStore(todoApp)}>
+  <Provider store={store}>
     <App />
   </Provider>,
   document.getElementById('root'));
