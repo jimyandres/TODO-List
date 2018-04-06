101a2305b1b82c4cb4ae631fa7a9eca7e37a6fa1
jimyandres <jimyandres@utp.edu.co>
2018-04-05 20:56:21 -0500

    Bug fixed: when using NavLink the active filter is updated correctly

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -4,25 +4,26 @@ import FilterTodos from '../FilterTodos';
 import ClearCompleted from '../ClearCompleted';
 import './index.css';
 
+const Footer = () =>
+  <div id="Footer">
+    <div id="Filters">
+      <FilterTodos visibility='all' title='All' />
+      <FilterTodos visibility='completed' title='Completed' />
+      <FilterTodos visibility='pending' title='Pending' />
+    </div>
+    <CompletedTasks/>
+    <ClearCompleted/>
+  </div>
+
 const mapStateToProps = (state) => {
   return {todos: state.todos,}
 };
 
-let Footer = ({todos}) => {
-  return (
-    <div id="Footer">
-      <div id="Filters">
-        <FilterTodos visibility='all' title='All' />
-        <FilterTodos visibility='completed' title='Completed' />
-        <FilterTodos visibility='pending' title='Pending' />
-      </div>
-      <div id="CompletedTasks" className="left">
-        {todos.filter(t => !t.completed).length} Items Left
-      </div>
-      <ClearCompleted/>
-    </div>
-  );
-};
-Footer = connect(mapStateToProps)(Footer);
+let CompletedTasks = ({todos}) =>
+  <div id="CompletedTasks" className="left">
+    {todos.filter(t => !t.completed).length} Items Left
+  </div>
+
+CompletedTasks = connect(mapStateToProps)(CompletedTasks);
 
 export default Footer;
