1aa6dc4ba210eb4e6163fd028f77595bf4c7c244
jimyandres <jimyandres@utp.edu.co>
2018-04-07 15:40:12 -0500

    The Middleware Chain for dispatch the actions

--- src/configureStore.js
+++ src/configureStore.js
@@ -1,41 +1,50 @@
 import { createStore } from 'redux';
 import todoApp from './redux';
 
-const addLoggingToDispatch = (store) => {
-  const rawDispatch = store.dispatch;
-  if (!console.group) {
-    return rawDispatch;
-  }
-
-  return (action) => {
-    console.group(action.type);
-    console.log('%c prev state', 'color: gray', store.getState());
-    console.log('%c action', 'color: yellow', action);
-    const returnValue = rawDispatch(action);
-    console.log('%c next state', 'color: green', store.getState());
-    console.groupEnd(action.type);
-    return returnValue;
-  };
-};
-
-const addPromiseSupportTodDispatch = (store) => {
-  const rawDispatch = store.dispatch;
-  return (action) => {
-    if (typeof action.then === 'function') {
-      return action.then(rawDispatch);
+const logger = (store) => {
+  return (next) => {
+    if (!console.group) {
+      return next;
     }
-    return rawDispatch(action);
+
+    return (action) => {
+      console.group(action.type);
+      console.log('%c prev state', 'color: gray', store.getState());
+      console.log('%c action', 'color: yellow', action);
+      const returnValue = next(action);
+      console.log('%c next state', 'color: green', store.getState());
+      console.groupEnd(action.type);
+      return returnValue;
+    };
   };
 };
+
+const promise = (store) => {
+  return (next) => {
+    return (action) => {
+      if (typeof action.then === 'function') {
+        return action.then(next);
+      }
+      return next(action);
+    };
+  };
+};
+
+const wrapDispatchWithMiddlewares = (store, middlewares) => {
+  middlewares.slice().reverse().forEach(middleware =>
+    store.dispatch = middleware(store)(store.dispatch)
+  );
+}
 
 const configureStore = () => {
   const store = createStore(todoApp);
+  const middlewares = [promise];
 
   if (process.env.NODE_ENV !== 'production') {
-    store.dispatch = addLoggingToDispatch(store);
+    middlewares.push(logger);
   }
 
-  store.dispatch = addPromiseSupportTodDispatch(store);
+  wrapDispatchWithMiddlewares(store, middlewares);
 
   return store;
 };
