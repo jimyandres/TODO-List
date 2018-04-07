1ee407bce9ca684bf343abb125ce351bfd10e0a8
jimyandres <jimyandres@utp.edu.co>
2018-04-07 13:50:58 -0500

    Simulate a data fetch from an API

---
+++ src/api/index.js
@@ -0,0 +1,33 @@
+import { v4 } from 'uuid'; // generation of RFC4122 UUIDS. Version 4 (random)
+
+// This is a fake in-memory implementation of something that would be implemented
+// by calling a REST server.
+
+const fakeDatabase = {
+  todos: [{
+    id: v4(),
+    text: 'hey',
+    completed: false,
+  },{
+    id: v4(),
+    text: 'ho',
+    completed: true,
+  }],
+};
+
+const delay = (ms) =>
+  new Promise(resolve => setTimeout(resolve, ms));
+
+export const fetchTodos = (filter) =>
+  delay(500).then(() => {
+    switch (filter) {
+      case 'all':
+        return fakeDatabase.todos;
+      case 'pending':
+        return fakeDatabase.todos.filter(t => !t.completed);
+      case 'completed':
+        return fakeDatabase.todos.filter(t => t.completed);
+      default:
+        throw new Error(`unknown filter: ${filter}`);
+    }
+  });
