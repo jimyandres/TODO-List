cee0cb58a228c17dc96dc05d1af00256ca5c6def
jimyandres <jimyandres@utp.edu.co>
2018-04-15 12:38:05 -0500

    Some changes in structure

--- src/components/Root/Root.js
+++ src/components/Root/Root.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import App from './App';
+import App from '../App';
 import { BrowserRouter, Route } from 'react-router-dom';
 
 const Root = ({store}) =>
