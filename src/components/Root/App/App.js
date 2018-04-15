8e7c89a30c1f643004574ef8f8d3b2ec35f3fbfd
jimyandres <jimyandres@utp.edu.co>
2018-04-15 11:44:15 -0500

    Refactory from App

---
+++ src/components/Root/App/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './index.css';
+
+import Header from './Header';
+import CreateTodo from './CreateTodo';
+import TodoListWithFilter from './TodoListWithFilter';
+import Footer from './Footer';
+
+const App = () =>
+  <div className="App">
+    <div className='AppTitle'>
+      <h1>TO DO</h1>
+    </div>
+
+    <div>
+      <Header>
+        <CreateTodo />
+      </Header>
+      <TodoListWithFilter />
+    </div>
+    <Footer />
+  </div>
+
+export default App;
