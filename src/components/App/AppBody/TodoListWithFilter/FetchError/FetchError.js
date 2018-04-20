dc01c403f94f6e5474942d717d79dd6edc70f107
jimyandres <jimyandres@utp.edu.co>
2018-04-19 20:32:12 -0500

    Fetch todos error message stylized

--- src/components/App/AppBody/TodoListWithFilter/FetchError/FetchError.js
+++ src/components/App/AppBody/TodoListWithFilter/FetchError/FetchError.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import './FetchError.css';
 
 const FetchError = ({ message, onRetry }) => (
-  <div>
+  <div className="FetchError">
     <p>Could not fetch todos. {message}</p>
-    <button onClick={onRetry}>Retry</button>
+    <button onClick={onRetry} className="btn FetchError">Retry</button>
   </div>
 );
 
