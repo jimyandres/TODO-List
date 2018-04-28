9a28fe5fed324d9e73a720a99a1b970363e9fa85
jimyandres <jimyandres@utp.edu.co>
2018-04-28 09:26:04 -0500

    Schema updated to support mongodb data

--- src/actionCreators/schema.js
+++ src/actionCreators/schema.js
@@ -1,4 +1,4 @@
 import { schema } from 'normalizr';
 
-export const todo = new schema.Entity('todos');
+export const todo = new schema.Entity('todos', undefined, { idAttribute: item => item._id.toString() });
 export const arrayOfTodos = [todo];
